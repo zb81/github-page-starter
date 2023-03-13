678476e42d19ab4aa566d9c15cb5e285b90b098a
CoderZB <zbneversettle@gmail.com>
2023-03-13 16:07:36 +0800

    fix(App.tsx): img url

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/github-page-starter
+/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
